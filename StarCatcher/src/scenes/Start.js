821ece793d448849fa22d4925d7fcae7f2d4e174
Robert <156192702+RobertIanCabilogan@users.noreply.github.com>
2026-05-13 18:13:10 +0800

    as

--- StarCatcher/src/scenes/Start.js
+++ StarCatcher/src/scenes/Start.js
@@ -13,10 +13,28 @@ export class Start extends Phaser.Scene {
     }
     
     create() {
-
         
         this.add.image(400, 300, 'Sky');
         this.platforms = this.physics.add.staticGroup();
+        this.player = this.physics.add.sprite(100, 450, 'Man').setScale(0.2);
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('Man', { start: 0, end: 1 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('Man', { start: 0, end: 1 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
         
         
         this.platforms.create(400, 750, 'Ground').setScale(6).refreshBody();
@@ -24,7 +42,23 @@ export class Start extends Phaser.Scene {
         this.platforms.create(600, 500, 'Ground');
         this.platforms.create(50, 340, 'Ground');
         this.platforms.create(1050, 345, 'Ground');
-        this.playerControl()
+
+        this.Stars = this.physics.add.group({
+            key: 'Star',
+            repeat: 11,
+            setXY: { x: 15, y: 0, stepX: 100 }
+        });
+
+        this.Stars.children.iterate(function (child) {
+
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+        child.setScale(0.06)
+        });
+        this.score = 0;
+        this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+        this.physics.add.collider(this.player, this.platforms);
+        this.physics.add.collider(this.Stars, this.platforms);
+        this.physics.add.overlap(this.player,this.Stars,this.collectStar,null,this);
 
     }
 
@@ -56,29 +90,10 @@ export class Start extends Phaser.Scene {
             this.player.setVelocityY(-330);
         }
     }
-
-
-    playerControl(){
-        this.player = this.physics.add.sprite(100, 450, 'Man').setScale(0.2);
-        this.cursors = this.input.keyboard.createCursorKeys();
-        this.player.setBounce(0.2);
-        this.player.setCollideWorldBounds(true);
-
-        this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers('Man', { start: 0, end: 1 }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers('Man', { start: 0, end: 1 }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.physics.add.collider(this.player, this.platforms);
+    collectStar (player, star)
+    {
+        star.disableBody(true, true);
+        this.score += 10;
+        this.scoreText.setText('Score: ' + this.score);
     }
-    
 }
